d6672b6e20e0986d822065593128b9d4a0deb309
Nikita Kurochkin <nikita.kurochkinn@gmail.com>
2018-12-07 17:36:04 +0500

    Add generate doc

--- example/generate.sh
+++ example/generate.sh
@@ -1,1 +1,6 @@
-protoc --go_out=plugins=grpc:. --jsonrpc-gateway_out=. service/*.proto
+protoc \
+  --go_out=plugins=grpc:.   \
+  --jsonrpc-gateway_out=.   \
+  --doc_out=.               \
+  --doc_opt=html,index.html \
+  service/*.proto
